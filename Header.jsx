e8924894a6c5a7d43c7e431df3f17b761b217785
devlopernabin <77477530+devlopernabin@users.noreply.github.com>
2021-04-05 11:58:10 +0545

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+const Header=(props)=>{
+
+    return(
+
+
+        <>
+        <header>
+
+<h1>{props.title} </h1>
+
+
+        </header>
+
+
+
+        </>
+
+    );
+
+    }
+Header.defaultProps={
+
+
+    title:'Task Tracker',
+}    
+
+Header.propTypes={
+
+title:PropTypes.number.isRequired,
+
+}
+
+
+export default Header
